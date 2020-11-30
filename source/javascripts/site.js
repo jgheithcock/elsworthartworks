782b749b53d0bd635d7d6e3037800c79712dcea7
JG Heithcock <jg.heithcock@retrospect.com>
2020-11-29 22:59:33 -0800

    Fix issue where Cmd+arrow wouldn't work, using tavern-wenches code
    (Which is simpler and better all around)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,26 +1,15 @@
-// This is where it all goes :)
+// From https://tavern-wenches.com/
 
-function goto(rel_link) {
-    if (rel_link && rel_link.length > 0)
-        window.location.href = $(rel_link)[0].href;
-}
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-            goto($("[rel$='prev']"));
-        break;
-
-        // case 38: // up
-        //break;
-
-        case 39: // right
-            goto($("[rel$='next']"));
-        break;
-
-        // case 40: // down
-        // break;
-
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  switch (keyName) {
+    case "ArrowLeft":
+      document.getElementsByClassName('prev_link')[0].click();
+      return;
+    case "ArrowRight":
+      document.getElementsByClassName('next_link')[0].click();
+      return;
+    default:
+      return;
+  }
 });
